c41e3edfa772f351122b659844fcabc35e9cba21
dariipavez <darielsuarez2014@gmail.com>
2024-11-16 15:19:21 -0300

    usuario modificado
    se envia el usuario_id para la actualización de la contraseña desde el front

--- api/usuario.js
+++ api/usuario.js
@@ -87,7 +87,7 @@ router.post("/login",function(req,res,next){
             console.log('Inicio Correctamente');
             const token = generarToken(TOKEN_SECRET, 6, { usuario_id: result[0].id, usuario: nombre_usuario, rol: result[0].rol });
             console.log(token);
-            return res.json({ status: 'ok', token });
+            return res.json({ status: 'ok', token , usuario_id: result[0].id,});
         } else {
             console.error('Usuario/Contraseña incorrecto');
             return res.status(403).json({ status: 'error', error: 'Usuario/Contraseña incorrecto' });
@@ -105,6 +105,11 @@ router.put('/actualizar/:id', function(req, res) {
     }
 
     const token = req.headers.authorization;
+
+    if(!token){
+        res.status(401).json({status:"error", eror:"no se identificó un token"})
+    }
+    
     const verificacion = verificarToken(token, TOKEN_SECRET);
 
     if (!verificacion?.data) {
