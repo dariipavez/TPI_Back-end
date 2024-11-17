669080392b919c22eb5ba38c155c96394ad4c0d9
dariipavez <darielsuarez2014@gmail.com>
2024-11-17 11:59:42 -0300

    ruta verificar datos y ver perfil

--- api/rutasUsuario.js
+++ api/rutasUsuario.js
@@ -38,7 +38,29 @@ router.get('/ver/producto_compra/:id?', function(req, res) {
     });
 });
 
-router.get('/ver/envio/:id?', function(req, res, next) {
+router.get('/ver/perfil/:id', function (req, res) {
+    const id = req.params.id;
+    const sql = "SELECT nombre_completo,mail, fecha_nac, nombre_usuario,telefono FROM usuario WHERE id = ?";
+    
+    conexion.query(sql, [id], function (error, result) {
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Error en la consulta' });
+        }
+        
+        if (result.length === 0) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+        
+        res.json({
+            status: 'ok',
+            usuario: result[0]
+        });
+    });
+});
+
+
+router.get('/ver/envio/:id?', function(req, res) {
     const id = req.params.id;
     const sql = id ? "SELECT * FROM envio WHERE id=?" : "SELECT * FROM envio";
 
